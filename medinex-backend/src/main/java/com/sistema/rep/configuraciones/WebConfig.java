dec31995822c4ca81970a23ba9de958f37232bed
JeremyVera2005 <Ashtoneslardox2005@gmail.com>
2025-11-14 13:35:48 -0500

    Refactor package name and update CORS configuration

--- medinex-backend/src/main/java/com/sistema/rep/configuraciones/WebConfig.java
+++ medinex-backend/src/main/java/com/sistema/rep/configuraciones/WebConfig.java
@@ -1,4 +1,4 @@
-package com.tuapp.config;
+package com.sistema.rep.configuraciones;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,12 +15,13 @@ public class WebConfig {
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
                         .allowedOrigins(
-                            "https://leafy-fudge-2633b7.netlify.app",
-                            "https://*.netlify.app",
-                            "http://localhost:4200"
+                                "https://leafy-fudge-2633b7.netlify.app",
+                                "https://*.netlify.app",
+                                "http://localhost:4200"
                         )
                         .allowedMethods("GET","POST","PUT","DELETE","OPTIONS")
                         .allowedHeaders("*")
+                        .exposedHeaders("Authorization")
                         .allowCredentials(true);
             }
         };
